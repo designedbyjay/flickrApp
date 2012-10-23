004aff3944146c6e03117d0cc69652592dc2196f
Jay Ramirez <ramij345@newschool.edu>
2012-10-22 23:38:16 -0400

    11:38pm

--- js/flickr.js
+++ js/flickr.js
@@ -1,38 +1,41 @@
 $(document).ready(function(){
 
-	alert(" ");
+	//alert(" ");
 
 	var key = 'fe16383c25c52c98d6d42b79fc58eb81'; 
 	var api_secret = 'cd06c0e9eee1b448';
 
-	var url = 'http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=' +key+ '&format=json&nojsoncallback=1';
-
+	var url = 'http://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key='+key+'&group_id=30649004@N00&format=json&nojsoncallback=1';
 	var imgSrc = new Array;
 
 	$.getJSON(url, function(json){
 
 		//console.log(json);
 		//console.log(json.photos);
-		console.log(json.photos.photo)
+		//console.log(json.photos.photo)
 		
 		// Flickr's img src URl format 
 		// http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
 
+		console.log(json);
+
 
 		for (var j = 0; j< json.photos.photo.length; j++){
 		
-		//console.log(j);
 
+		/* */
 		var farm = json.photos.photo[j].farm;
 		var id = json.photos.photo[j].id;
 		var server = json.photos.photo[j].server;
 		var secret = json.photos.photo[j].secret;
 		var title = json.photos.photo[j].title;
 
-		var img = 'http://farm'+farm+'.staticflickr.com/'+server+'/'+id+'_'+secret+'_q.jpg'
-
-		var image = $('<img/>').attr('src', img).attr('alt', title).appendTo('#container');
-
+		
+	var img = 'http://farm'+farm+'.staticflickr.com/'+server+'/'+id+'_'+secret+'_q.jpg'
+console.log(img);
+	var title = $('#container').append("<p>"+title+"</p>");	
+	var image = $('<img/>').attr('src', img).attr('alt', title).appendTo('#container');
+	
 		}
 
 
